1715bf931612e279306b57b4d7cf698d9861c655
Zahidul Islam <zahidislam2806@gmail.com>
2025-03-17 18:32:09 +0600

    Address class added

---
+++ TestProjectGitHub/Model/Addreess.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProjectGitHub.Model
+{
+    public class Addreess
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
