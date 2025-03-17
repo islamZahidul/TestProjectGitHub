1886bd42058a1b5fb56511332c7be1b7f5c9fa6a
Zahidul Islam <zahidislam2806@gmail.com>
2025-03-17 19:07:54 +0600

    Book Class added

---
+++ TestProjectGitHub/Model/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProjectGitHub.Model
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Brand { get; set; }
+        
+    }
+}
